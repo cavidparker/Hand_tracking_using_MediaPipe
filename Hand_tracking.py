5ec449e35fd543e54aed43ec445a6534a62fbeb1
cavidparker <cavidpark@gmail.com>
2022-01-24 11:55:04 +0600

    point landmark added in the hand

---
+++ Hand_tracking.py
@@ -0,0 +1,24 @@
+import cv2
+import mediapipe as mp
+import numpy as np
+import time
+
+cap = cv2.VideoCapture(0)
+mpHand = mp.solutions.hands
+hands = mpHand.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+while True:
+    success, img = cap.read()
+    # img = cv2.resize(img, (640, 480))
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+    print(results.multi_hand_landmarks)
+
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img, handLms)
+
+    cv2.imshow('img', img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
